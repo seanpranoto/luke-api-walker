43b781969952fa818a97864e89c8c0fa89025032
seanmp19 <68677836+seanmp19@users.noreply.github.com>
2020-09-23 22:59:49 +0700

    axios pokemon api assignment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [pokemons , setPokemons] = useState([]);
   return (
     <div >
-      <Form setPokemons={setPokemons}/>\
+      <Form setPokemons={setPokemons}/>
       <Content pokemons={pokemons}/>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Form = ({setPokemons}) => {
-    const [data, setData]=useState([]);
+
+    const [data, setData]=useState(null);
+
     useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
-            .then(response => response.json())
-            .then(response => setData(response.results))
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+            .then(response => setData(response.data.results))
+            .catch(err=>console.log(err))
     }, []);
 
     const onClick=()=> setPokemons(data);
